f35937646111023f8de701b1d5a2608886ff6f6c
Dominic Coelho <dominic.coelho@formidable.com>
2019-04-18 16:25:32 +0100

    basic link creation slash navigation

--- routes/home/index.js
+++ routes/home/index.js
@@ -3,42 +3,47 @@ const styles = css`/routes/home/index.css`;
 
 export default () => {
   const [code, setCode] = React.useState([]);
+  const query =
+    window.location.href.split(`entry=`)[1].replace(/\/$/, "") ||
+    "lodash-es@4.17.11/lodash.js";
+  const queryForEntry = query.replace(/(?<=\/)[^\/]*$/, "").replace(/\/$/, "");
 
   React.useEffect(() => {
-    fetch("https://unpkg.com/lodash-es@4.17.11/lodash.js")
+    fetch(`https://unpkg.com/${query}`)
       .then(res => res.text())
-      // .then(res =>
-      //   res.replace(
-      //     /(?<=from ')\.\/(.*)(?=';)/,
-      //     `<a href='https://unpkg.com/lodash-es@4.17.11/$1'>$1</a>`
-      //   )
-      // )
-      // .then(res => res.split(/(?<=from ')(?=\.\/.*\'\;)/))
       .then(res => res.split(/(\.\/\w*\.js)/))
       .then(res => setCode(res));
 
-    console.log("cDM");
+    console.log("cDM", "query", query, "2", queryForEntry);
   }, []);
   console.log("rendering", code);
   return html`
     <div className=${styles}>
       <header className="App-header">
         <${Logo} className="App-logo" />
-        <p>Edit <code>routes/home.js</code> and save to reload.</p>
+        <p>
+          Click ${" "}
+          <a
+            onClick=${() => {
+              window.history.go(-1);
+            }}
+          >
+            here
+          </a>
+          ${" "} to go up a level
+        </p>
         <pre>
           ${code.map(x => {
             return x.startsWith(`./`)
               ? html`
                   <a
-                    href="${x.replace(
-                      "./",
-                      `https://unpkg.com/lodash-es@4.17.11/`
-                    )}"
+                    key=${x}
+                    href="${x.replace("./", `/?entry=${queryForEntry}/`)}"
                     >${x}</a
                   >
                 `
               : html`
-                  <span>${x}</span>
+                  <span key=${x}>${x}</span>
                 `;
           })}
         </pre
