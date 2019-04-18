1bc3a1523b6193ac962e70850b4e60d01624a2d2
kiraarghy <karaestubbs@gmail.com>
2019-04-18 18:06:40 +0100

    More styling changes for landing page.

--- routes/home/index.js
+++ routes/home/index.js
@@ -14,7 +14,7 @@ export default () => {
   const packageName = query.split(/\@|\//)[0];
   const queryForEntry = query.replace(/(?<=\/)[^\/]*$/, "").replace(/\/$/, "");
 
-  const [displayedThing, toggleDisplayed] = React.useState(true);
+  const [displayedOverlay, toggleDisplayed] = React.useState(false);
 
   React.useEffect(() => {
     fetch(`https://unpkg.com/${query}`)
@@ -31,70 +31,82 @@ export default () => {
     console.log("cDM", "query", query, "2", queryForEntry);
   }, []);
 
+  React.useEffect(() => {
+    if (displayedOverlay) {
+      window.localStorage.setItem("displayedOverlay", true);
+    }
+  }, [displayedOverlay]);
+
   console.log("rendering", code);
   return html`
     <div className=${styles}>
-      ${displayedThing
+      ${!displayedOverlay && !window.localStorage.displayedOverlay
         ? html`
             <div className="Overlay"><h1 className="Overlay-Text">Formidable</div>
+            <${ProjectBadge} color="#ca5688" abbreviation="De" description="Dora Explorer" number="43"/>
             <button className="Overlay-Button"
-                onClick=${e => toggleDisplayed(false)}>
-                Remove
+                onClick=${() => toggleDisplayed(true)}>
+                Start Exploring
             </button>
-            <${ProjectBadge} color="#ca5688" abbreviation="De" description="Dora the Package Explorer" number="43"/>
             </div>
+            
           `
-        : null}
-      <div className="App-Body">
-        <div className="Editor">
-          <header className="App-header">
-            <${Logo} className="App-logo" />
-            <p>
-              Click ${" "}
-              <a
-                onClick=${() => {
-                  window.history.go(-1);
-                }}
-              >
-                here
-              </a>
-              ${" "} to go up a level
-            </p>
-            <pre>
+        : html`
+            <div className="App-Body">
+              <div className="Editor">
+                <header className="App-header">
+                  <${Logo} className="App-logo" />
+                  <p>
+                    Click ${" "}
+                    <a
+                      onClick=${() => {
+                        window.history.go(-1);
+                      }}
+                    >
+                      here
+                    </a>
+                    ${" "} to go up a level
+                  </p>
+                  <pre>
           ${code.map(x => {
-                return x.startsWith(`./`)
-                  ? html`
-                      <a
-                        key=${x}
-                        href="${x.replace("./", `/?entry=${queryForEntry}/`)}"
-                        >${x}</a
-                      >
-                    `
-                  : html`
-                      <span key=${x}>${x}</span>
-                    `;
-              })}
-        </pre>
-          </header>
-        </div>
-        <div className="SideBar">
-          <span className="Info"
-            >Package: ${meta.packageName ? meta.packageName : null}</span
-          >
+                      return x.startsWith(`./`)
+                        ? html`
+                            <a
+                              key=${x}
+                              href="${x.replace(
+                                "./",
+                                `/?entry=${queryForEntry}/`
+                              )}"
+                              >${x}</a
+                            >
+                          `
+                        : html`
+                            <span key=${x}>${x}</span>
+                          `;
+                    })}
+        </pre
+                  >
+                </header>
+              </div>
+              <div className="SideBar">
+                <span className="Info"
+                  >Package: ${meta.packageName ? meta.packageName : null}</span
+                >
 
-          <span className="Info"
-            >Size: ${meta.size ? meta.size : null} bytes</span
-          >
+                <span className="Info"
+                  >Size: ${meta.size ? meta.size : null} bytes</span
+                >
 
-          <span className="Info"
-            >Number imports: ${meta.imports ? meta.imports : null}</span
-          >
+                <span className="Info"
+                  >Number imports: ${meta.imports ? meta.imports : null}</span
+                >
 
-          <span className="Info"
-            >Number exports: ${meta.exports ? meta.exports : null}</span
-          >
-        </div>
-      </div>
+                <span className="Info"
+                  >Number exports: ${meta.exports ? meta.exports : null}</span
+                >
+              </div>
+            </div>
+          `}
     </div>
   `;
 };
