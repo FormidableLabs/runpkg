894ee3c313935b7b5775aca890658d5ce14d3951
Luke Jackson <lukejacksonn@gmail.com>
2019-04-18 18:36:17 +0100

    Main ui cleanup

--- routes/home/index.js
+++ routes/home/index.js
@@ -1,20 +1,19 @@
-import Logo from "../../components/logo.js";
-import ProjectBadge from "../../components/ProjectBadge.js";
+import ProjectBadge from '../../components/ProjectBadge.js'
 
-const styles = css`/routes/home/index.css`;
+const styles = css`/routes/home/index.css`
 
 export default () => {
-  const [code, setCode] = React.useState([]);
-  const [meta, setMeta] = React.useState({});
+  const [code, setCode] = React.useState([])
+  const [meta, setMeta] = React.useState({})
 
-  const query = window.location.search.includes("entry=")
-    ? window.location.href.split(`entry=`)[1].replace(/\/$/, "")
-    : "lodash-es@4.17.11/lodash.js";
+  const query = window.location.search.includes('entry=')
+    ? window.location.href.split(`entry=`)[1].replace(/\/$/, '')
+    : 'lodash-es@4.17.11/lodash.js'
 
-  const packageName = query.split(/\@|\//)[0];
-  const queryForEntry = query.replace(/(?<=\/)[^\/]*$/, "").replace(/\/$/, "");
+  const packageName = query.split(/\@|\//)[0]
+  const queryForEntry = query.replace(/(?<=\/)[^\/]*$/, '').replace(/\/$/, '')
 
-  const [displayedThing, toggleDisplayed] = React.useState(true);
+  const [displayedThing, toggleDisplayed] = React.useState(true)
 
   React.useEffect(() => {
     fetch(`https://unpkg.com/${query}`)
@@ -24,77 +23,38 @@ export default () => {
           size: res.length,
           packageName,
           imports: (res.match(/import/g) || []).length,
-          exports: (res.match(/export/g) || []).length
-        });
-        setCode(res.split(/(\.\/\w*\.js)/));
-      });
-    console.log("cDM", "query", query, "2", queryForEntry);
-  }, []);
+          exports: (res.match(/export/g) || []).length,
+        })
+        setCode(res.split(/(\.\/\w*\.js)/))
+      })
+  }, [])
 
-  console.log("rendering", code);
   return html`
-    <div className=${styles}>
-      ${displayedThing
-        ? html`
-            <div className="Overlay"><h1 className="Overlay-Text">Formidable</div>
-            <button className="Overlay-Button"
-                onClick=${e => toggleDisplayed(false)}>
-                Remove
-            </button>
-            <${ProjectBadge} color="#ca5688" abbreviation="De" description="Dora the Package Explorer" number="43"/>
-            </div>
-          `
-        : null}
-      <div className="App-Body">
-        <div className="Editor">
-          <header className="App-header">
-            <${Logo} className="App-logo" />
-            <p>
-              Click ${" "}
-              <a
-                onClick=${() => {
-                  window.history.go(-1);
-                }}
-              >
-                here
-              </a>
-              ${" "} to go up a level
-            </p>
-            <pre>
+    <main class=${styles}>
+      <article>
+        <pre>
           ${code.map(x => {
-                return x.startsWith(`./`)
-                  ? html`
-                      <a
-                        key=${x}
-                        href="${x.replace("./", `/?entry=${queryForEntry}/`)}"
-                        >${x}</a
-                      >
-                    `
-                  : html`
-                      <span key=${x}>${x}</span>
-                    `;
-              })}
-        </pre>
-          </header>
-        </div>
-        <div className="SideBar">
-          <span className="Info"
-            >Package: ${meta.packageName ? meta.packageName : null}</span
-          >
-
-          <span className="Info"
-            >Size: ${meta.size ? meta.size : null} bytes</span
-          >
-
-          <span className="Info"
-            >Number imports: ${meta.imports ? meta.imports : null}</span
-          >
-
-          <span className="Info"
-            >Number exports: ${meta.exports ? meta.exports : null}</span
-          >
-        </div>
-      </div>
-    </div>
-  `;
-};
+            return x.startsWith(`./`)
+              ? html`
+                  <a
+                    key=${x}
+                    href="${x.replace('./', `/?entry=${queryForEntry}/`)}"
+                    >${x}</a
+                  >
+                `
+              : html`
+                  <span key=${x}>${x}</span>
+                `
+          })}
+        </pre
+        >
+      </article>
+      <aside>
+        <h1>${meta.packageName}</h1>
+        <span>${meta.size} bytes</span>
+        <span>${meta.imports} Imports</span>
+        <span>${meta.exports} Exports</span>
+      </aside>
+    </main>
+  `
+}
