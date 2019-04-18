08c9385cdc1dbbf4489ce52187e85c79cbf95233
Dominic Coelho <dominic.coelho@formidable.com>
2019-04-18 15:07:12 +0100

    initial push - example with lodash

--- routes/home/index.js
+++ routes/home/index.js
@@ -1,19 +1,49 @@
-import Logo from '../../components/logo.js'
-const styles = css`/routes/home/index.css`
+import Logo from "../../components/logo.js";
+const styles = css`/routes/home/index.css`;
 
-export default () => html`
-  <div className=${styles}>
-    <header className="App-header">
-      <${Logo} className="App-logo" />
-      <p>Edit <code>routes/home.js</code> and save to reload.</p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
-    </header>
-  </div>
-`
+export default () => {
+  const [code, setCode] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch("https://unpkg.com/lodash-es@4.17.11/lodash.js")
+      .then(res => res.text())
+      // .then(res =>
+      //   res.replace(
+      //     /(?<=from ')\.\/(.*)(?=';)/,
+      //     `<a href='https://unpkg.com/lodash-es@4.17.11/$1'>$1</a>`
+      //   )
+      // )
+      // .then(res => res.split(/(?<=from ')(?=\.\/.*\'\;)/))
+      .then(res => res.split(/(\.\/\w*\.js)/))
+      .then(res => setCode(res));
+
+    console.log("cDM");
+  }, []);
+  console.log("rendering", code);
+  return html`
+    <div className=${styles}>
+      <header className="App-header">
+        <${Logo} className="App-logo" />
+        <p>Edit <code>routes/home.js</code> and save to reload.</p>
+        <pre>
+          ${code.map(x => {
+            return x.startsWith(`./`)
+              ? html`
+                  <a
+                    href="${x.replace(
+                      "./",
+                      `https://unpkg.com/lodash-es@4.17.11/`
+                    )}"
+                    >${x}</a
+                  >
+                `
+              : html`
+                  <span>${x}</span>
+                `;
+          })}
+        </pre
+        >
+      </header>
+    </div>
+  `;
+};
