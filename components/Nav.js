e7475cb3e6fb46c9b7f12d65c722a00eb1c1b0f3
Dominic Coelho <dominic.coelho@formidable.com>
2019-05-08 17:02:21 +0100

    merge conflict fixesm and minor refactor

--- components/Nav.js
+++ components/Nav.js
@@ -1,52 +1,53 @@
-import { html } from 'https://unpkg.com/rplus';
+import { html, react } from 'https://unpkg.com/rplus';
 import NpmLogo from './NpmLogo.js';
 import FolderIcon from './FolderIcon.js';
 import FileIcon from './FileIcon.js';
 import MenuIcon from './MenuIcon.js';
 import CloseIcon from './CloseIcon.js';
 
-const File = ({ meta, parent }) => html`
-  <li key=${meta.path} style=${{ order: 1 }}>
-    ${FileIcon}
-    <a onClick=${() => pushState(`?${request.package}${meta.path}`)}
-      >${meta.path.replace(parent.path, '')}
-    </a>
-  </li>
-`;
-
-const Directory = ({ rootMeta }) => html`
-  <ul style=${{ order: 0 }}>
-    ${rootMeta.path &&
-      rootMeta.path !== '/' &&
-      html`
-        <div>
-          ${FolderIcon}
-          <h2>${rootMeta.path.slice(1)}</h2>
-        </div>
-      `}
-    ${rootMeta.files.map(meta =>
-      meta.type === 'file'
-        ? File({ meta, parent: rootMeta })
-        : Directory({ rootMeta: meta })
-    )}
-  </ul>
-`;
+const pushState = url => history.pushState(null, null, url);
 
 export default ({ packageJSON, packageMeta }) => {
-  const [navShowing, showNav] = react.useState(false);
-
+  const [isNavShowing, showNav] = react.useState(true);
   const { name, version, main, license, description } = packageJSON;
   const packageMainUrl = `?${name}@${version}/${main}`;
   const npmUrl = 'https://npmjs.com/' + packageJSON.name;
 
+  const File = ({ meta, parent }) => html`
+    <li key=${meta.path} style=${{ order: 1 }}>
+      ${FileIcon}
+      <a onClick=${() => pushState(`?${name}@${version}${meta.path}`)}
+        >${meta.path.replace(parent.path, '')}
+      </a>
+    </li>
+  `;
+
+  const Directory = ({ rootMeta }) => html`
+    <ul style=${{ order: 0 }}>
+      ${rootMeta.path &&
+        rootMeta.path !== '/' &&
+        html`
+          <div>
+            ${FolderIcon}
+            <h2>${rootMeta.path.slice(1)}</h2>
+          </div>
+        `}
+      ${rootMeta.files.map(meta =>
+        meta.type === 'file'
+          ? File({ meta, parent: rootMeta })
+          : Directory({ rootMeta: meta })
+      )}
+    </ul>
+  `;
+
   return html`
-    <button className="toggleOpenButton" onClick=${() => showNav(true)}>
-      ${MenuIcon}
+    <button
+      className="toggleOpenButton"
+      onClick=${() => showNav(!isNavShowing)}
+    >
+      ${isNavShowing ? CloseIcon : MenuIcon}
     </button>
-    <nav className=${navShowing ? 'active' : 'hiding'}>
-      <button className="toggleCloseButton" onClick=${() => showNav(false)}>
-        ${CloseIcon}
-      </button>
+    <nav className=${isNavShowing ? 'active' : 'hiding'}>
       <h1 onClick=${() => pushState(packageMainUrl)} data-test="title">
         ${name}
       </h1>
