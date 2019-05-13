c6d02350677f8351512f3fac203fbe45a0d49e11
Luke Jackson <lukejacksonn@gmail.com>
2019-05-13 12:30:07 +0100

    Weekend work (#61)
    * Small tidy up, applied doms comment stripping fixes
    
    * Better caching and handling of scoped packages like @babel/core
    
    * Only recursiveDependencyFetch when given a resolved file url
    
    * Strip trailing / from all request urls
    
    * Never try recursiveDependencyFetch a non resolved URL
    
    * Prevent text overflowing in aside panel
    
    * Fix spelling error on 404 screen
    
    * Update strip comments regex

--- components/Aside.js
+++ components/Aside.js
@@ -6,7 +6,7 @@ import FileIcon from './FileIcon.js';
 
 const pushState = url => history.pushState(null, null, url);
 
-const FileList = ({ title, files, cache, packageName }) => html`
+const FileList = ({ title, files, packageName }) => html`
   <div key=${title}>
     <h3>${title}</h3>
     <span>${files.length} Files</span>
@@ -14,39 +14,79 @@ const FileList = ({ title, files, cache, packageName }) => html`
   <ul key=${files.join('-')}>
     ${files.map(
       x => html`
-        <li key=${x} data-test=${title + 'Item'}>
+        <li key=${x.url} data-test=${title + 'Item'}>
           ${FileIcon}
           <a
             onClick=${e => {
               e.preventDefault();
-              pushState(`?${x.replace('https://unpkg.com/', '')}`);
+              pushState(`?${x.url.replace('https://unpkg.com/', '')}`);
             }}
           >
-            <span>
-              ${x.replace(`https://unpkg.com/`, '').replace(packageName, '')}
-            </span>
-            <span>${formatBytes(cache[x].code.length)}</span>
+            ${x.url.replace(`https://unpkg.com/`, '').replace(packageName, '')}
           </a>
+          <span>${formatBytes(x.size)}</span>
         </li>
       `
     )}
   </ul>
 `;
 
-export default ({ packageJSON, request }) => {
+// const fetchDependencies = all => key =>
+//   all[key].dependencies.reduce(
+//     (deps, dep) => ({
+//       ...deps,
+//       [dep]: all[dep],
+//       ...(all[key].dependencies ? fetchDependencies(all)(dep) : {}),
+//     }),
+//     {}
+//   );
+
+export default ({ file }) => {
   const [cache, setCache] = react.useState({});
 
-  // Runs when file changes + fetches dependencies.
   react.useEffect(() => {
-    if (packageJSON.name && request.file) {
-      setCache({});
-      /* Fetch all files in this module */
-      console.log(`Recursively fetching ${request.url}`);
-      recursiveDependencyFetch(packageJSON, request.url).then(setCache);
+    setCache({});
+    // Match breaks this ?enhanced-resolve@4.1.0/lib/NodeJsInputFileSystem
+    // No match breaks preact@8.4.2/devtools
+    if (file.url) {
+      console.log(`Analysing ${file.url}`);
+      recursiveDependencyFetch(file.url).then(setCache);
     }
-  }, [packageJSON.name, request.file]);
-  const file = cache[`https://unpkg.com/${request.url}`];
-  const { name, version } = packageJSON;
+  }, [file.url]);
+
+  const target = cache[file.url];
+
+  if (!target)
+    return html`
+      <aside key="aside">
+        ${Spinner}
+      </aside>
+    `;
+
+  const { name, version } = file.pkg;
+  // const deepDependencies = fetchDependencies(cache)(key);
+
+  const externalDependencies = target.dependencies
+    .filter(x => !x.includes(`${name}@${version}`))
+    .map(x => cache[x]);
+
+  const internalDependencies = target.dependencies
+    .filter(x => x.includes(`${name}@${version}`))
+    .map(x => cache[x]);
+
+  const knownDependants = target.dependants.map(x => cache[x]);
+  // <div key="imported-size">
+  //   <h3>Imported Size</h3>
+  //   <span
+  //     >${formatBytes(
+  //       target.code.length +
+  //         Object.values(deepDependencies).reduce(
+  //           (a, b) => a + b.code.length,
+  //           0
+  //         )
+  //     )}</span
+  //   >
+  // </div>
 
   return html`
     <aside key="aside">
@@ -57,34 +97,27 @@ export default ({ packageJSON, request }) => {
               This is all the information we have derived from the contents of
               this file as well as any previously explored files.
             </p>
-            <div key="size">
-              <h3>Individual File Size</h3>
-              <span>${formatBytes(file.code.length)}</span>
+            <div key="filesize">
+              <h3>File Size</h3>
+              <span>${formatBytes(target.size)}</span>
             </div>
             <${FileList}
               title="External Dependencies"
-              files=${file.dependencies.filter(
-                x => !x.includes(`${name}@${version}`)
-              )}
-              cache=${cache}
-              packageName=${`${name}@${version}`}
+              files=${externalDependencies}
               key="external_dependencies"
+              packageName=${`${name}@${version}`}
             />
             <${FileList}
-              title="Local Dependencies"
-              files=${file.dependencies.filter(x =>
-                x.includes(`${name}@${version}`)
-              )}
-              cache=${cache}
+              title="Internal Dependencies"
+              files=${internalDependencies}
+              key="internal_dependencies"
               packageName=${`${name}@${version}`}
-              key="local_dependencies"
             />
             <${FileList}
               title="Known Dependants"
-              files=${file.dependants}
-              cache=${cache}
-              packageName=${`${name}@${version}`}
+              files=${knownDependants}
               key="dependants"
+              packageName=${`${name}@${version}`}
             />
           `
         : Spinner}
--- components/ErrorBlock404.js
+++ components/ErrorBlock404.js
@@ -7,8 +7,8 @@ const ErrorBlock404 = setFetchErrorStatus => html`
     <div className="error">
       <h1 data-test="Error-Title">404</h1>
       <p>
-        Oh no! It looks like you're trying to find a package that doesn't exist.
-        Please check that its name is spelled correctly.
+        Oh no, we couldn't find that package! Check the package name was spelt
+        correctly or create an issue.
       </p>
       <button
         data-test="Error-Button"
