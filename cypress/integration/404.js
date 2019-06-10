627fba785c9657fccd0d6f013f2d5fbdaf911341
kiraarghy <karaestubbs@gmail.com>
2019-06-10 15:35:55 +0100

    Add cypress tests

--- cypress/integration/404.js
+++ cypress/integration/404.js
@@ -13,10 +13,6 @@ describe('Runpkg', () => {
   });
 
   it('Visits the 404 page', () => {
-    cy.visit(url);
-    cy.wait(3000);
-    cy.get('[data-test=Overlay-Button]', { timeout: 10000 }).click();
-    cy.url().should('include', '/?lodash-es');
     cy.visit(url + '/?loadashhhhhh');
     cy.get('[data-test=Error-Title]', { timeout: 10000 }).contains('404');
     cy.get('[data-test=Error-Button]', { timeout: 10000 }).click();
