c427dbdcdeea3af2e050cc1b5cd314f7b39a642c
kiraarghy <karaestubbs@gmail.com>
2019-11-11 12:32:18 +0000

    kara/nonstandardversionfix
    1. fix code review comments.
    1. add tests using esm package.

--- tests/utils/parseUrl.test.js
+++ tests/utils/parseUrl.test.js
@@ -5,89 +5,93 @@ const parseUrlFile = require('../../utils/parseUrl');
 const parseUrl = parseUrlFile.parseUrl;
 
 describe('parseUrl', () => {
-  it('parses non beta package', () => {
-    expect(parseUrl('https://unpkg.com/pkg-dash@1.0.0/extra.js')).toEqual({
-      name: 'pkg-dash',
-      version: '1.0.0',
-      path: '/pkg-dash@1.0.0/extra.js',
-      file: 'extra.js',
-      directory: null,
+    it('parses non beta package', () => {
+        expect(parseUrl('https://unpkg.com/pkg-dash@1.0.0/extra.js')).toEqual({
+            name: 'pkg-dash',
+            version: '1.0.0',
+            path: '/pkg-dash@1.0.0/extra.js',
+            file: 'extra.js',
+            directory: null
+        });
     });
-  });
-  it('parses beta package', () => {
-    expect(
-      parseUrl('https://unpkg.com/pkg-dash@1.0.0-beta.1/extra.js')
-    ).toEqual({
-      name: 'pkg-dash',
-      version: '1.0.0-beta.1',
-      path: '/pkg-dash@1.0.0-beta.1/extra.js',
-      file: 'extra.js',
-      directory: null,
+    it('parses beta package', () => {
+        expect(
+            parseUrl('https://unpkg.com/pkg-dash@1.0.0-beta.1/extra.js')
+        ).toEqual({
+            name: 'pkg-dash',
+            version: '1.0.0-beta.1',
+            path: '/pkg-dash@1.0.0-beta.1/extra.js',
+            file: 'extra.js',
+            directory: null
+        });
     });
-  });
-  it('parses beta package with file a directory down', () => {
-    expect(
-      parseUrl('https://unpkg.com/fast-deep-equal@3.0.0-beta.1/es6/index.js')
-    ).toEqual({
-      file: 'index.js',
-      name: 'fast-deep-equal',
-      path: '/fast-deep-equal@3.0.0-beta.1/es6/index.js',
-      version: '3.0.0-beta.1',
-      directory: 'es6',
+    it('parses beta package with file a directory down', () => {
+        expect(
+            parseUrl(
+                'https://unpkg.com/fast-deep-equal@3.0.0-beta.1/es6/index.js'
+            )
+        ).toEqual({
+            file: 'index.js',
+            name: 'fast-deep-equal',
+            path: '/fast-deep-equal@3.0.0-beta.1/es6/index.js',
+            version: '3.0.0-beta.1',
+            directory: 'es6'
+        });
     });
-  });
-  it('parses scoped non-beta package', () => {
-    expect(parseUrl('https://unpkg.com/@scope/pkg@1.0.0/index.js')).toEqual({
-      file: 'index.js',
-      name: '@scope/pkg',
-      path: '/@scope/pkg@1.0.0/index.js',
-      directory: null,
-      version: '1.0.0',
+    it('parses scoped non-beta package', () => {
+        expect(parseUrl('https://unpkg.com/@scope/pkg@1.0.0/index.js')).toEqual(
+            {
+                file: 'index.js',
+                name: '@scope/pkg',
+                path: '/@scope/pkg@1.0.0/index.js',
+                directory: null,
+                version: '1.0.0'
+            }
+        );
     });
-  });
-  it('parses scoped non-beta package with file a directory down', () => {
-    expect(parseUrl('https://unpkg.com/@scope/pkg@1.0.0/es6/index.js')).toEqual(
-      {
-        file: 'index.js',
-        name: '@scope/pkg',
-        path: '/@scope/pkg@1.0.0/es6/index.js',
-        directory: 'es6',
-        version: '1.0.0',
-      }
-    );
-  });
-  it('parses scoped beta package', () => {
-    expect(
-      parseUrl('https://unpkg.com/@scope/pkg@1.0.0-beta.1/index.js')
-    ).toEqual({
-      file: 'index.js',
-      name: '@scope/pkg',
-      path: '/@scope/pkg@1.0.0-beta.1/index.js',
-      directory: null,
-      version: '1.0.0-beta.1',
+    it('parses scoped non-beta package with file a directory down', () => {
+        expect(
+            parseUrl('https://unpkg.com/@scope/pkg@1.0.0/es6/index.js')
+        ).toEqual({
+            file: 'index.js',
+            name: '@scope/pkg',
+            path: '/@scope/pkg@1.0.0/es6/index.js',
+            directory: 'es6',
+            version: '1.0.0'
+        });
     });
-  });
-  it('keys are returned null if not available', () => {
-    expect(parseUrl('https://unpkg.com/')).toEqual({
-      file: null,
-      name: null,
-      path: null,
-      directory: null,
-      version: null,
+    it('parses scoped beta package', () => {
+        expect(
+            parseUrl('https://unpkg.com/@scope/pkg@1.0.0-beta.1/index.js')
+        ).toEqual({
+            file: 'index.js',
+            name: '@scope/pkg',
+            path: '/@scope/pkg@1.0.0-beta.1/index.js',
+            directory: null,
+            version: '1.0.0-beta.1'
+        });
     });
-    expect(parseUrl('https://unpkg.com/@scope/pkg')).toEqual({
-      file: null,
-      name: '@scope/pkg',
-      path: '/@scope/pkg',
-      directory: null,
-      version: null,
+    it('keys are returned null if not available', () => {
+        expect(parseUrl('https://unpkg.com/')).toEqual({
+            file: null,
+            name: null,
+            path: null,
+            directory: null,
+            version: null
+        });
+        expect(parseUrl('https://unpkg.com/@scope/pkg')).toEqual({
+            file: null,
+            name: '@scope/pkg',
+            path: '/@scope/pkg',
+            directory: null,
+            version: null
+        });
+        expect(parseUrl('https://unpkg.com/@scope/pkg@1.0.0-beta.1')).toEqual({
+            file: null,
+            name: '@scope/pkg',
+            path: '/@scope/pkg@1.0.0-beta.1',
+            directory: null,
+            version: '1.0.0-beta.1'
+        });
     });
-    expect(parseUrl('https://unpkg.com/@scope/pkg@1.0.0-beta.1')).toEqual({
-      file: null,
-      name: '@scope/pkg',
-      path: '/@scope/pkg@1.0.0-beta.1',
-      directory: null,
-      version: '1.0.0-beta.1',
-    });
-  });
 });
--- utils/recursiveDependencyFetch.js
+++ utils/recursiveDependencyFetch.js
@@ -7,41 +7,41 @@ const UNPKG = 'https://unpkg.com/';
 const isExternalPath = str => !str.startsWith('.');
 const isLocalFile = str => !isExternalPath(str) && fileNameRegEx.test(str);
 const stripComments = str =>
-  str.replace(/^[\t ]*\/\*(.|\r|\n)*?\*\/|^[\t ]*\/\/.*/gm, '');
+    str.replace(/^[\t ]*\/\*(.|\r|\n)*?\*\/|^[\t ]*\/\/.*/gm, '');
 
 const extractDependencies = (input, pkg) => {
-  const code = stripComments(input);
+    const code = stripComments(input);
 
-  const imports = (
-    code.match(/^(import|export).*(from)[ \n]+['"](.*?)['"];?$/gm) || []
-  ).map(x => x.match(/^(import|export).*(from)[ \n]+['"](.*?)['"];?$/)[3]);
+    const imports = (
+        code.match(/^(import|export).*(from)[ \n]+['"](.*?)['"];?$/gm) || []
+    ).map(x => x.match(/^(import|export).*(from)[ \n]+['"](.*?)['"];?$/)[3]);
 
-  const requires = (code.match(/(require\(['"])[^)\n\r]*(['"]\))/gm) || [])
-    .map(x => x.match(/['"](.*)['"]/)[1])
-    .filter(
-      x =>
-        !isExternalPath(x) ||
-        // Allows both @babel/core and proptypes/something
-        Object.keys(pkg.dependencies || {}).includes(
-          x.startsWith('@') ? x : x.split('/')[0]
-        )
+    const requires = (code.match(/(require\(['"])[^)\n\r]*(['"]\))/gm) || [])
+        .map(x => x.match(/['"](.*)['"]/)[1])
+        .filter(
+            x =>
+                !isExternalPath(x) ||
+                // Allows both @babel/core and proptypes/something
+                Object.keys(pkg.dependencies || {}).includes(
+                    x.startsWith('@') ? x : x.split('/')[0]
+                )
+        );
+
+    // Return array of unique dependencies appending js
+    // extension to any relative imports that have no extension
+    return [...new Set([...imports, ...requires])].map(x =>
+        isExternalPath(x) || isLocalFile(x) ? x : `${x}.js`
     );
-
-  // Return array of unique dependencies appending js
-  // extension to any relative imports that have no extension
-  return [...new Set([...imports, ...requires])].map(x =>
-    isExternalPath(x) || isLocalFile(x) ? x : `${x}.js`
-  );
 };
 
 const packageJsonUrl = path => {
-  const { name, version } = parseUrl(path);
-  if (!name) {
-    console.error(`Unable to parse path: ${path}`);
-    return undefined;
-  } else {
-    return `${UNPKG}${name}@${version}/package.json`;
-  }
+    const { name, version } = parseUrl(path);
+    if (!name) {
+        console.error(`Unable to parse path: ${path}`);
+        return undefined;
+    } else {
+        return `${UNPKG}${name}@${version}/package.json`;
+    }
 };
 
 // cache keeps memory of what was run last
@@ -52,19 +52,19 @@ const cache = {};
 // returns it, if not it fetches it.
 
 class PkgCache {
-  constructor() {
-    this.cache = new Map();
-  }
-  async fetchPkg(key) {
-    if (this.cache.has(key)) {
-      return await this.cache.get(key);
+    constructor() {
+        this.cache = new Map();
     }
-    const resultPromise = fetch(key)
-      .then(fetch(key))
-      .then(res => res.json());
-    this.cache.set(key, resultPromise);
-    return await resultPromise;
-  }
+    async fetchPkg(key) {
+        if (this.cache.has(key)) {
+            return await this.cache.get(key);
+        }
+        const resultPromise = fetch(key)
+            .then(fetch(key))
+            .then(res => res.json());
+        this.cache.set(key, resultPromise);
+        return await resultPromise;
+    }
 }
 
 // FileCache is a class that maintains a cache of files we've fetched
@@ -72,82 +72,84 @@ class PkgCache {
 // returns it, if not it fetches it.
 
 class FileCache {
-  constructor() {
-    this.cache = new Map();
-  }
-  async fetchFiles(key) {
-    if (this.cache.has(key)) {
-      return await this.cache.get(key);
+    constructor() {
+        this.cache = new Map();
     }
-    const resultPromise = fetch(key).then(async res => ({
-      url: res.url,
-      code: await res.text(),
-    }));
-    this.cache.set(key, resultPromise);
-    return await resultPromise;
-  }
+    async fetchFiles(key) {
+        if (this.cache.has(key)) {
+            return await this.cache.get(key);
+        }
+        const resultPromise = fetch(key).then(async res => ({
+            url: res.url,
+            code: await res.text()
+        }));
+        this.cache.set(key, resultPromise);
+        return await resultPromise;
+    }
 }
 
 /* eslint-disable max-statements, max-params */
 const recursiveDependantsFetch = async (path, parent, fileCache, pkgCache) => {
-  if (!fileCache) {
-    fileCache = new FileCache();
-  }
-  if (!pkgCache) {
-    pkgCache = new PkgCache();
-  }
-  const { url, code } = await fileCache.fetchFiles(path);
-  const pkg = await pkgCache.fetchPkg(packageJsonUrl(url));
-
-  // If we asked for a file but got redirected by unpkg
-  // then update the url in the parents dependencies
-  if (cache[parent] && path !== url) {
-    const position = cache[parent].dependencies.map(x => x[1]).indexOf(path);
-    cache[parent].dependencies[position][1] = url;
-  }
-
-  // Checks if we've already fetched the file and dependencies
-  // and doesn't fetch it again.
-  if (cache[url]) {
-    // If this file requests file 'x' but we've already
-    // requested file 'x' then it infers that this file
-    // is a parent of file 'x'.
-    if (parent) {
-      cache[url] = {
-        ...cache[url],
-        dependants: [...cache[url].dependants, parent],
-      };
+    if (!fileCache) {
+        fileCache = new FileCache();
     }
-    return;
-  }
+    if (!pkgCache) {
+        pkgCache = new PkgCache();
+    }
+    const { url, code } = await fileCache.fetchFiles(path);
+    const pkg = await pkgCache.fetchPkg(packageJsonUrl(url));
 
-  const dependencies = extractDependencies(code, pkg).map(x => [
-    x,
-    makePath(url)(x),
-  ]);
+    // If we asked for a file but got redirected by unpkg
+    // then update the url in the parents dependencies
+    if (cache[parent] && path !== url) {
+        const position = cache[parent].dependencies
+            .map(x => x[1])
+            .indexOf(path);
+        cache[parent].dependencies[position][1] = url;
+    }
 
-  cache[url] = {
-    url,
-    size: code.length,
-    dependencies,
-    dependants: parent ? [parent] : [],
-  };
+    // Checks if we've already fetched the file and dependencies
+    // and doesn't fetch it again.
+    if (cache[url]) {
+        // If this file requests file 'x' but we've already
+        // requested file 'x' then it infers that this file
+        // is a parent of file 'x'.
+        if (parent) {
+            cache[url] = {
+                ...cache[url],
+                dependants: [...cache[url].dependants, parent]
+            };
+        }
+        return;
+    }
 
-  // Then we call the function again for all dependencies of
-  // that file and wait for return.
-  // eslint-disable-next-line consistent-return
-  return Promise.all(
-    dependencies.map(x =>
-      recursiveDependantsFetch(x[1], url, fileCache, pkgCache)
-    )
-  );
+    const dependencies = extractDependencies(code, pkg).map(x => [
+        x,
+        makePath(url)(x)
+    ]);
+
+    cache[url] = {
+        url,
+        size: code.length,
+        dependencies,
+        dependants: parent ? [parent] : []
+    };
+
+    // Then we call the function again for all dependencies of
+    // that file and wait for return.
+    // eslint-disable-next-line consistent-return
+    return Promise.all(
+        dependencies.map(x =>
+            recursiveDependantsFetch(x[1], url, fileCache, pkgCache)
+        )
+    );
 };
 /* eslint-enable max-statements*/
 
 export default async entry => {
-  // Start tree walking dependencies from the given entry point
-  // otherwise start from the projects main entry point
-  await recursiveDependantsFetch(entry);
-  // Returns new cache.
-  return cache;
+    // Start tree walking dependencies from the given entry point
+    // otherwise start from the projects main entry point
+    await recursiveDependantsFetch(entry);
+    // Returns new cache.
+    return cache;
 };
