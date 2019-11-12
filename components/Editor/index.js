04b37a122fdfac8de04984e2e7984c281e23d252
Alex Saunders <alex.saunders@formidable.com>
2019-11-12 15:37:09 +0000

    add dependency links

--- components/Editor/index.js
+++ components/Editor/index.js
@@ -1,89 +1,91 @@
-import { html, css } from 'https://unpkg.com/rplus-production@1.0.0';
+import { react, html, css } from 'https://unpkg.com/rplus-production@1.0.0';
 import Highlight, {
-  defaultProps,
+  Prism,
 } from 'https://unpkg.com/prism-react-renderer?module';
 
-// import pushState from '../utils/pushState.js';
+import Link from '../Link.js';
 
-// redirects req from unpkg -> runpkg
-// const handleEditorOnClick = e => {
-//   const href = e.nativeEvent.target.parentNode.href;
-//   if (href) {
-//     e.preventDefault();
-//     pushState(href.replace('https://unpkg.com/', '?'));
-//   }
-// };
+export default ({ value, dependencyState, url, ...rest }) => {
+  const dependencies = react.useMemo(
+    () => dependencyState[url] && dependencyState[url].dependencies,
+    [dependencyState, url]
+  );
 
-// const handleCtrlDown = e => {
-//   if (e.metaKey || e.ctrlKey) {
-//     document.querySelectorAll('.imports').forEach(x => x.classList.add('ctrl'));
-//   }
-// };
+  const findTokenDependency = react.useCallback(
+    token => {
+      if (!dependencies) return null;
+      const strippedDependency = token.replace(/['"]+/g, '');
+      const dependency = dependencies.find(
+        dep => dep[0] === strippedDependency
+      );
+      return dependency;
+    },
+    [dependencies]
+  );
 
-// const handleCtrlUp = e => {
-//   if (e.key === 'Meta' || e.key === 'Control') {
-//     document
-//       .querySelectorAll('.imports')
-//       .forEach(x => x.classList.remove('ctrl'));
-//   }
-// };
-
-// this function maps over dependencies and appends
-// anchor tags to imports in the editor
-// const anchorAppender = deps => {
-//   deps.forEach(y => {
-//     const imports = [...document.querySelectorAll('.token.string')].find(x =>
-//       x.innerText.includes(y[0])
-//     );
-//     if (!imports) {
-//       return;
-//     }
-//     const clonedImports = imports.cloneNode(true);
-//     clonedImports.classList.add('imports');
-//     const anchor = document.createElement('a');
-//     anchor.href = y[1];
-//     anchor.appendChild(clonedImports);
-//     imports.replaceWith(anchor);
-//   });
-//   return;
-// };
-
-// const extensions = {
-//   js: 'javascript',
-//   re: 'reason',
-//   json: 'json',
-// };
-
-const styles = css`
-  line-height: 138%;
-  font-family: 'Inconsolata', monospace;
-  padding: 42px;
-`;
-
-// eslint-disable-next-line no-unused-vars
-const theme = css`/theme.css`;
-
-export default ({ value, ...rest }) => {
   return html`
-  <${Highlight} ...${defaultProps} code=${value} language="javascript" theme=${undefined}>
-    ${({ className, style, tokens, getLineProps, getTokenProps }) => html`
-      <pre className=${`${styles} ${className}`} style=${style} ...${rest}>
-        ${tokens.map(
-          (line, i) => html`
-            <div ...${getLineProps({ line, key: i })}>
-              ${line.map(
-                (token, key) => html`
-                  <span ...${getTokenProps({ token, key })} />
-                `
-              )}
-            </div>
-          `
-        )}
+    <${Highlight}
+      Prism=${Prism}
+      code=${value}
+      language="javascript"
+      theme=${undefined}
+    >
+      ${({ className, style, tokens, getLineProps, getTokenProps }) => html`
+        <pre
+          className=${`${styles.container} ${className}`}
+          style=${style}
+          ...${rest}
+        >
+        ${tokens.map((line, i) => {
+            const isImportExportLine = line.some(
+              token =>
+                token.types.includes('module') ||
+                (token.types.includes('function') &&
+                  token.content === 'require')
+            );
+
+            return html`
+              <div ...${getLineProps({ line, key: i })}>
+                ${line.map((token, key) => {
+                  const dependency =
+                    isImportExportLine &&
+                    token.types.includes('string') &&
+                    findTokenDependency(token.content);
+
+                  return dependency
+                    ? html`
+                        <${Link}
+                          href=${`/${dependency[1].replace(
+                            'https://unpkg.com/',
+                            '?'
+                          )}`}
+                          className=${styles.link}
+                        >
+                          <span ...${getTokenProps({ token, key })}
+                        /><//>
+                      `
+                    : html`
+                        <span ...${getTokenProps({ token, key })} />
+                      `;
+                })}
+              </div>
+            `;
+          })}
       </pre
-      >
-    `}
-  </${Highlight}>
+        >
+      `}
+    <//>
   `;
 };
 
-/*eslint-enable */
+const styles = {
+  container: css`
+    line-height: 138%;
+    font-family: 'Inconsolata', monospace;
+    padding: 42px;
+  `,
+  link: css`
+    text-decoration: underline;
+    text-decoration-color: #f8b1f1;
+  `,
+};
