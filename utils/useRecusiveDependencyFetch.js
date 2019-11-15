1ceb6465d02db03a68bb96823f6905453424d5bc
kiraarghy <karaestubbs@gmail.com>
2019-11-15 16:27:14 +0000

    kara/webworker
    Fix multiple re-renders and so spinning up of multiple webworkers.

--- utils/useRecusiveDependencyFetch.js
+++ utils/useRecusiveDependencyFetch.js
@@ -1,30 +1,37 @@
 import { react } from 'https://unpkg.com/rplus-production@1.0.0';
 
 export const useRecusiveDependencyFetch = (
-  createWorker,
   input,
   callbackToSetState,
   callBackToDispatch
   // eslint-disable-next-line max-params
 ) => {
   console.log(`Analysing ${input}`);
-  const worker = react.useMemo(createWorker, [createWorker]);
+  const inputRef = react.useRef(null);
   const lastWorker = react.useRef(null);
   react.useEffect(() => {
-    lastWorker.current = worker;
-    worker.postMessage(input);
-    return () => {
-      worker.terminate();
-    };
-  }, [worker, callbackToSetState, callBackToDispatch]);
+    if (input !== inputRef.current) {
+      lastWorker.current = new Worker(
+        './utils/recursiveDependencyFetchWorker.js'
+      );
+      inputRef.current = input;
+      lastWorker.current.postMessage(input);
+    }
+    return () => (lastworker.current ? lastWorker.current.terminate() : null);
+  }, [lastWorker]);
   react.useEffect(() => {
-    lastWorker.current.addEventListener('message', e => {
-      callbackToSetState(e.data);
-      callBackToDispatch(e.data);
-    });
-    return lastWorker.current.removeEventListener('message', e => {
-      callbackToSetState(e.data);
-      callBackToDispatch(e.data);
-    });
-  }, [callBackToDispatch, callbackToSetState]);
+    if (lastWorker.current) {
+      lastWorker.current.addEventListener('message', e => {
+        callbackToSetState(e.data);
+        callBackToDispatch(e.data);
+      });
+    }
+    return () => {
+      lastWorker.current.removeEventListener('message', e => {
+        callbackToSetState(e.data);
+        callBackToDispatch(e.data);
+      });
+    };
+  }, [callBackToDispatch, callbackToSetState, lastWorker]);
+  return null;
 };
