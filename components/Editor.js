21a04ae28bfb53ac247598eb9041e39b02f71e72
Alex Saunders <alex.saunders@formidable.com>
2019-11-19 14:48:17 +0000

    add blur/opacity loading state to editor

--- components/Editor.js
+++ components/Editor.js
@@ -1,4 +1,4 @@
-import { html, css } from 'https://unpkg.com/rplus-production@1.0.0';
+import { react, html, css } from 'https://unpkg.com/rplus-production@1.0.0';
 import Highlight, {
   Prism,
 } from 'https://unpkg.com/prism-react-renderer?module';
@@ -20,20 +20,55 @@ const hasImport = line =>
 
 const removeQuotes = packageName => packageName.replace(/['"]+/g, '');
 
+function usePrevious(value) {
+  const ref = react.useRef();
+  react.useEffect(() => {
+    ref.current = value;
+  }, [value]);
+  return ref.current;
+}
+
 export default () => {
   const [{ code, cache, request }] = useStateValue();
   const selectedLine = getSelectedLineNumberFromUrl();
-  const { dependencies } = cache['https://unpkg.com/' + request.path] || {};
-  if (!dependencies) return null;
+  const fileData = cache['https://unpkg.com/' + request.path];
+
+  const prevCode = usePrevious(code);
+  const prevReq = usePrevious(request);
+  const prevFileData = react.useRef();
+  react.useEffect(() => {
+    prevFileData.current = fileData || prevFileData.current;
+  }, [fileData]);
+
+  let loading = false;
+
+  let codeToRender = code;
+  let deps = fileData && fileData.dependencies;
+  if (
+    (prevReq && prevReq.path !== request.path) ||
+    !(fileData && fileData.dependencies)
+  ) {
+    codeToRender = prevCode;
+    deps = prevFileData.current && prevFileData.current.dependencies;
+    loading = true;
+  }
+
+  if (!deps) {
+    return null;
+  }
   return html`
     <${Highlight}
       Prism=${Prism}
-      code=${code.slice(0, 100000)}
+      code=${codeToRender.slice(0, 100000)}
       language="javascript"
       theme=${undefined}
     >
       ${({ className, style, tokens, getLineProps, getTokenProps }) => html`
-        <pre className=${`${styles.container} ${className}`} style=${style}>
+        <pre
+          className=${`${styles.container} ${className} ${loading &&
+            styles.loading}`}
+          style=${style}
+        >
         ${tokens.map((line, i) => {
             const isImportLine = hasImport(line);
             return html`
@@ -50,7 +85,7 @@ export default () => {
                   const dep =
                     isImportLine &&
                     token.types.includes('string') &&
-                    dependencies[removeQuotes(token.content)];
+                    deps[removeQuotes(token.content)];
                   return dep
                     ? html`
                         <${Link}
@@ -79,7 +114,13 @@ const styles = {
     line-height: 138%;
     font-family: 'Inconsolata', monospace;
     padding: 2rem 1rem;
-    overflow: scroll;
+    transition: all 0.3s cubic-bezier(0.4, 0, 0.2, 1);
+  `,
+  loading: css`
+    opacity: 0.5;
+    -webkit-filter: blur(1px); /* Safari */
+    filter: blur(1px);
+    transition-delay: 0.2s;
   `,
   link: css`
     text-decoration: underline;
