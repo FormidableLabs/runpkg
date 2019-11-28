160aa10be1fdde78d9317eb9f3d253a6806561bf
kiraarghy <karaestubbs@gmail.com>
2019-11-28 10:54:33 +0000

    Fix cypress rebase

--- cypress/integration/integration_spec.js
+++ cypress/integration/integration_spec.js
@@ -3,7 +3,7 @@ describe('Runpkg', () => {
   before(() => {
     url =
       Cypress.env('env') === 'local'
-        ? 'https://localhost:8080'
+        ? 'http://localhost:8080'
         : 'https://runpkg.com';
   });
   it('Visits the page', () => {
