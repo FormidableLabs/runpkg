a14310cf3d711540edda7b507eed7ae1ac78214d
Haroen Viaene <fingebimus@me.com>
2021-08-18 18:38:02 +0200

    fix: add Algolia logo (#200)
    * fix: add Algolia logo
    
    This is required under our usage of the npm-search index, so that other people can find out how to use it too
    
    * add link to logo

--- utils/rplus.js
+++ utils/rplus.js
@@ -5,7 +5,9 @@ import htm from 'htm';
 import css from 'csz';
 import search from 'algolia';
 
+// https://www.algolia.com/doc/faq/accounts-billing/do-i-need-to-display-the-algolia-logo-when-i-am-on-the-free-plan/
 const npm = search('OFCNCOG2CU', '86ebd6a34c7fbb7988d5ba5c75d5da34');
+
 const html = htm.bind(React.createElement);
 const react = {
   ...React,
